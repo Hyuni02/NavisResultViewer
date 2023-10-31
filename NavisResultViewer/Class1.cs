a9624621d5f0a89f0a774f2f9d5b506dda26cc80
Hyuni02 <jackjsh@naver.com>
2023-10-31 19:59:26 +0900

    attribute추가

--- NavisResultViewer/Class1.cs
+++ NavisResultViewer/Class1.cs
@@ -4,16 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using wf = System.Windows.Forms;
 using Autodesk.Navisworks.Api;
 using Autodesk.Navisworks.Api.Clash;
 using Autodesk.Navisworks.Api.Plugins;
 
-namespace NavisResultViewer
-{
+namespace NavisResultViewer {
+    [PluginAttribute("ResultViewer", "Hyuni & Dotman", DisplayName = "ResultViewer", ToolTip = "halloween")]
     public class MainClass : AddInPlugin {
-        public override int Execute(params string[] parameters) {
 
+
+        public override int Execute(params string[] parameters) {
+            ShowMessage("ResultViewer Clicked");
             return 0;
         }
+
+        void ShowMessage(string content) {
+            wf.MessageBox.Show(content);
+        }
     }
+
+
 }
