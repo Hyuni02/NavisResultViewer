0c0d6674420a8e5057924468d037f465bcc6da47
Hyuni02 <jackjsh@naver.com>
2023-10-31 19:35:02 +0900

    init

---
+++ NavisResultViewer/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Autodesk.Navisworks.Api;
+using Autodesk.Navisworks.Api.Clash;
+using Autodesk.Navisworks.Api.Plugins;
+
+namespace NavisResultViewer
+{
+    public class MainClass : AddInPlugin {
+        public override int Execute(params string[] parameters) {
+
+            return 0;
+        }
+    }
+}
